7972f3f2b0fda5409e02b9ac8883875f2959fb42
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-24 17:33:14 -0600

    Add song titles to the musicPlaylist arraylist, and print it

--- src/Playlist/Playlist.java
+++ src/Playlist/Playlist.java
@@ -5,6 +5,15 @@ public class Playlist {
     public static void main(String[] args) {
         //create playlist arraylist
         ArrayList<String> musicPlaylist = new ArrayList<String>();
+        //add songs to playlist
+        musicPlaylist.add("That's What Friends are For");
+        musicPlaylist.add("Elderly Woman");
+        musicPlaylist.add("The New Year");
+        musicPlaylist.add("Love Like Rockets");
+        musicPlaylist.add("Dear Prudence");
+        musicPlaylist.add("Wings For Marie");
+        musicPlaylist.add("Iin Desu Ka?");
 
+        System.out.println(musicPlaylist);
     }
 }
