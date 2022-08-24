3872871deb3d15e0ba9750bd1ec4ec5b01b98932
kushmirc <107213928+kushmirc@users.noreply.github.com>
2022-08-24 17:30:25 -0600

    Import ArrayList utility and add musicPlaylist arraylist

--- src/Playlist/Playlist.java
+++ src/Playlist/Playlist.java
@@ -1,8 +1,10 @@
 package Playlist;
-
+import java.util.ArrayList;
 public class Playlist {
 
     public static void main(String[] args) {
+        //create playlist arraylist
+        ArrayList<String> musicPlaylist = new ArrayList<String>();
 
     }
 }
